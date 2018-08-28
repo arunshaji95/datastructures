114c4cc72df65f84a851f4a2bb2c19856ca6bc21
arunshaji <arunshaji@qburst.com>
2018-08-28 17:28:34 +0530

    Initial commit. Added stack data structure.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Data Structures in Python
+
+This repository illustrates the implementation of basic data structures in Python. Following are the currently implemented data structures.
+1. Stack
---
+++ stack.py
@@ -0,0 +1,62 @@
+
+class StackError(Exception):
+    pass
+
+
+class Stack(object):
+    """
+    Class representing the Stack data structure.
+    A stack is a linear data structure in which insertions and
+    deletions are performed only at one end. Hence it is called
+    Last In First Out(LIFO) data structure.
+    Eg: A stack of plates in a restaurant.
+    """
+
+    def __init__(self, max_size):
+        self.items = []
+        self.max_size = max_size
+
+    def push(self, value):
+        if len(self.items) >= self.max_size:
+            raise StackError('StackOverflow!!!!')
+        self.items.append(value)
+
+    def pop(self):
+        if not self.items:
+            raise StackError('StackUnderflow!!!')
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def print_stack(self):
+        if not self.items:
+            print('Stack is empty')
+            return
+        for i in self.items:
+            print(i, end=', ')
+        print()
+
+
+def main():
+    s = Stack(5)
+    while True:
+        try:
+            s.print_stack()
+            print('0. Exit\n1. Push\n2. Pop\n')
+            choice = int(input('Enter your choice: '))
+            if choice == 0:
+                break
+            elif choice == 1:
+                value = int(input('Enter the value to be inserted: '))
+                s.push(value)
+            elif choice == 2:
+                print('Poped value is {}'.format(s.pop()))
+            else:
+                print('Wrong Choice')
+        except StackError as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    main()
